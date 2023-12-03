29082196a7effb0d432346066351be9dcfcbe331
Ankit D <ankit.99d@gmail.com>
2023-12-03 22:33:34 +0530

    enhancements

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,15 +2,13 @@ import { Client, Account, Databases } from "appwrite";
 
 export const client = new Client();
 
-client
-    .setEndpoint("https://cloud.appwrite.io/v1")
-    .setProject(process.env.VITE_APPWRITE_PROJECT_ID!);
+client.setEndpoint("https://cloud.appwrite.io/v1").setProject("");
 
 export const account = new Account(client);
 export { ID } from "appwrite";
 
 const databases = new Databases(client);
-const databaseId = process.env.VITE_APPWRITE_DB_ID!;
-const collectionId = process.env.VITE_APPWRITE_COLLECTION_ID!;
-
-export { databases, databaseId, collectionId };
+const databaseId = "";
+const collectionId = "";
+const contactFormCollectionId = "";
+export { databases, databaseId, collectionId, contactFormCollectionId };
